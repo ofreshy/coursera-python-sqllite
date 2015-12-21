a06db9862c9b46861e10da0969f0acf1cda3732b
ofreshy <offer@magnetic.com>
2015-12-21 13:28:17 -0500

    done

---
+++ pythonsql/geo/__init__.py
@@ -0,0 +1,6 @@
+'''
+All the code in this lib was downloaded from :
+- http://www.pythonlearn.com/code/geodata.zip
+as part of the week 5 assignment of the coursera course
+- Using Databases with Python
+'''
